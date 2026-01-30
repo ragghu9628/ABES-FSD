b1d9011058c0966e614456a6125893399f25d519
Raghavendra <167605664+ragghu9628@users.noreply.github.com>
2026-01-30 10:28:40 +0530

    Transfer the data in another path

---
+++ Day2/readAndWrite.js
@@ -0,0 +1,12 @@
+import fs, { write } from 'fs'
+const writeFileSync=(pathd,ata)=>{
+        try{
+            fs.writeFileSync(path,data);
+            console.log("Data has written")
+        }
+        catch(error){
+console.log("unable to write the data");
+        }
+    }
+const readFileSync=();
+
