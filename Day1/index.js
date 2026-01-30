2f8be1720bf7a3b228bc933961b3e654307f65d6
Raghavendra <167605664+ragghu9628@users.noreply.github.com>
2026-01-30 09:32:07 +0530

    Added JS code to index.js

--- Day1/index.js
+++ Day1/index.js
@@ -1,9 +1,19 @@
 const myPromise = new Promise((resolve, reject) => {
-    let marks = 78;
+    let marks = 33;
     if (marks >= 33) {
         resolve("You passed!");
     } else {
         reject("You failed!");
     }
 })
-myPromise.then((msg) => console.log(msg)).catch((error) => console.log("some error occurred",error));
+const f1=async()=>{
+    try{
+        const msg =await myPromise;
+        console.log(msg);
+
+    }catch(error){
+        console.log(error)
+    }
+}
+// myPromise.then((msg) => console.log(msg)).catch((error) => console.log("some error occurred",error));
+f1();//Arrow function are not hoisted means we have to call after declear it
