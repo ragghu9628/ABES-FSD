96f3698ea527da20191392fbb6be8d1e10fe902b
Raghavendra <167605664+ragghu9628@users.noreply.github.com>
2026-02-12 12:39:21 +0530

    readwriteAsync

--- Day2/readAndWrite.js
+++ Day2/readAndWrite.js
@@ -1,12 +1,35 @@
-import fs, { write } from 'fs'
-const writeFileSync=(pathd,ata)=>{
-        try{
-            fs.writeFileSync(path,data);
-            console.log("Data has written")
-        }
-        catch(error){
-console.log("unable to write the data");
-        }
-    }
-const readFileSync=();
+import fs from 'fs';
 
+const appendFileSync = (path, data) => {
+    try {
+        fs.appendFileSync(file, data);
+        console.log("Data has been appended successfully.");
+    } catch (error) {
+        console.log("unable to append file");
+    }
+}
+appendFileSync("./example.txt", "This data has been appended through sync fun of fs");
+
+
+
+
+
+
+
+export const writeFileSync = (path, data) => {
+    try {
+        fs.writeFileSync(path, data);
+        console.log("Data has been successfully written...");
+    } catch (error) {
+        4
+        console.log("unable to write data");
+    }
+}
+export const readFileSync = (path) => {
+    try {
+        const data = fs.readFileSync(path, 'utf-8');
+        console.log(data, "Data has been read successfully...");
+    } catch (error) {
+        console.log("unable to read data");
+    }
+}
